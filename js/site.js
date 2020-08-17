281d92076ec7c02b57fa55f936bbf1fcc94f40c1
Andy Rea <email@andrewrea.co.uk>
2020-08-17 23:31:50 +0100

    Coming together now

--- js/site.js
+++ js/site.js
@@ -1,12 +1,40 @@
 Vue.component('value-item', {
   props: ["value"],
+  data: function(){
+    return {
+      oldValue: "",
+      value: "",
+      editing: false
+    }
+  },
   methods: {
     remove: function(e){
       this.$root.$emit('item-removed', this.value)
+    },
+    edit: function(e){
+      this.oldValue = this.value;
+      this.editing = true;
+    },
+    update: function(e){
+      this.editing = false;
+      this.$root.$emit('item-updated', {
+        new: this.value,
+        old: this.oldValue 
+      });
     }
   },
-  template: `<div class="row">
-          <div class="col">{{ value  }}</div>
+  template: `<div class="row" v-if="editing">
+          <div class="col">
+            <input type="text" v-model="value">
+          </div>
+          <div class="col">
+            <button v-on:click="update">
+              Update
+            </button>
+          </div>
+        </div>
+        <div class="row" v-else>
+          <div class="col" v-on:click="edit">{{ value  }}</div>
           <div class="col">
             <button v-on:click="remove">
               Remove
@@ -47,7 +75,10 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
 
+const router = new VueRouter({})
+
 let v = new Vue({ 
+  router,
   el: '#app',
   mounted(){
     this.$root.$on('item-removed', (e) => {
@@ -58,13 +89,29 @@ let v = new Vue({
         value: e
       })
     })
+    this.$root.$on('item-updated', (e) => {
+      this.items[this.items.findIndex(el => el.value === e.old)].value = e.new;
+      console.log(e);
+    })
+    if (this.$router.history.current.query.data){
+      var decodedValues=decodeURI(this.$router.history.current.query.data)
+      var json=JSON.parse(decodedValues)
+      this.items = json.map(e => {
+        return {value: e}
+      });
+
+    }
   },
   methods:{
-    generate: function(){
+    generate: function(e){
       this.value = this.items[getRandomInt(this.items.length)].value;
+      this.$router.push({ path:"/saved" , query: { data: encodeURI(JSON.stringify(this.items.map(e=>e.value)))  } })
+      this.url = "https://danrom.com/#"+this.$router.history.current.fullPath;
+      e.stopPropagation();
     }
   },
   data: {
+    url:"#",
     title: "INSERT TITLE HERE...",
     value: "Click generate ...",
     items: [{
